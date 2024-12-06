829ddd2b55bfd1e60d5f1694f159473b50781c70
yjgaia <yjdev88@gmail.com>
2024-12-06 20:18:58 +0900

    Update SVG dimensions in MaterialLoadingSpinner for responsive rendering

--- src/MaterialLoadingSpinner.ts
+++ src/MaterialLoadingSpinner.ts
@@ -4,6 +4,6 @@ export default class MaterialLoadingSpinner extends DomNode {
   constructor() {
     super(".loading-spinner.material-loading-spinner");
     this.htmlElement.innerHTML =
-      '<svg width="24px" height="24px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg"><circle fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle></svg>';
+      '<svg width="100%" height="100%" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg"><circle fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle></svg>';
   }
 }
